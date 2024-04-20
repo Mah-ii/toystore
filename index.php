356703fa9bf9258acb547cff08723447ada139da
Your Name <you@example.com>
2024-04-21 00:58:11 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="style.css">
+    <title>ToyStore</title>
+</head>
+<body>
+
+      <!--topbaar-->
+      <nav class="navbar-one flex">
+        <div class="left flex">
+            <div class="email">
+                <i class="fas fa-envelope"></i>
+                <span>toystore@gmail.com</span>
+            </div>
+
+            <div class="phone">
+            <i class="fas fa-phone"></i>
+                <span>+8801111111111</span>
+            </div>
+        </div>
+
+        <div class="right flex">
+            <div class="twitter">
+                <i class="fab fa-twitter"></i>
+            </div>
+            <div class="instagram">
+                <i class="fab fa-instagram"></i>
+            </div>
+            <div class="facebook">
+                <i class="fab fa-facebook"></i>
+            </div>
+            <div class="pinterest">
+                <i class="fab fa-pinterest"></i>
+            </div>
+            <div class="youtube">
+                <i class="fab fa-youtube"></i>
+            </div>
+        </div>
+    </nav>
+
+    <header>
+        <div class="logo">ToyStore</div>
+        <input type="checkbox" id="nav_check" hidden>
+        <nav>
+            <ul>
+                <li>
+                    <a href="" class="active">Catalog</a>
+                </li>
+                <li>
+                    <a href="">Delivery</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </nav>
+        <label for="nav_check" class="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </label>
+    </header>
+
+    <!-- main part -->
+
+    <section class="main_part1">
+        <div class="box">
+            <h4>Hello Everyone</h4>
+            <p>Welcome to the Toystore</p>
+            <button class="btn1">Open Catalog</button>
+        </div>
+    </section>
+</body>
+</html>
