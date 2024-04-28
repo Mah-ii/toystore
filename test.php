50b2084f8b53e527abc5500d97d5ab218acff1d8
Your Name <you@example.com>
2024-04-28 14:07:10 +0600

    2nd commit

--- test.php
+++ test.php
@@ -5,8 +5,131 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="test.css">
     <title>ToyStore</title>
+
+    <!-- <style>
+     
+        .main_part2 {
+            display: flex;
+            justify-content: space-between;
+            height: 550px;
+            width: 100%;
+        }
+
+        .part2 {
+            display: flex;
+            justify-content: space-between;
+            height: 400px;
+            position: relative;
+            
+        }
+
+        .box1, .box2 {
+            flex: 1;
+            margin: 0 25px; 
+            height: 200px;
+            width: 500px;
+            margin-top: 10rem;
+            border-radius: 15px;
+            position: relative; 
+            color: white; 
+        }
+
+        .box1{
+            margin-left: 10rem;
+            background-color:   #d2540d  ;
+        
+        }
+
+        .box1 h3{
+            text-align:right;
+            color: white;
+            font-weight: bold;
+            font-size: 35px;
+            margin-top: 2rem;
+            padding-right: 2.2rem;
+
+        }
+
+        .button2{
+            display: flex;
+            justify-content:right;
+
+        }
+
+
+        .btn2{
+            background-color: white;
+            color: black;
+            height: 35px;
+            width: 100px;
+            font-size: 15px;
+            border-radius: 20px;
+            cursor: pointer;
+        }
+
+        .box2 {
+            background-color: #DE3163;
+            display: flex;
+            justify-content: space-between;
+            position: relative;
+        }
+
+        .box2 h3 {
+            text-align: right; 
+            color: white;
+            font-weight: bold;
+            font-size: 35px;
+            margin-top: 2rem;
+            padding-right: 2rem;
+            position: absolute; 
+            right: 0;
+        }
+
+        .button3 {
+            display: flex;
+            justify-content: right;
+            position: absolute; 
+            right: 0;
+            top: 5rem;
+            padding-right: 5rem;
+        }
+
+        .btn3 {
+            background-color: white;
+            color: black;
+            height: 35px;
+            width: 100px;
+            font-size: 15px;
+            border-radius: 20px;
+            cursor: pointer;
+            margin-top: 0.5rem; 
+        }
+
+
+        img {
+            height: 13rem;
+            margin: 4rem;
+            position: relative;
+            z-index: 1;
+        }
+
+        .pic3{
+            position: absolute;
+            top: -95px;
+            z-index: 2;
+        }
+
+        .pic4{
+            position: relative;
+            top: -95px;
+            z-index: 2;
+        }
+
+
+
+    </style> -->
 
 
 </head>
